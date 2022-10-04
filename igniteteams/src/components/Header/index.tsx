b28066aa8c0fe57a11607c189f3b02ea85357c41
Lucca Hora <luccahora@gmail.com>
2022-10-04 19:31:35 -0300

    chore: back to home screen

--- igniteteams/src/components/Header/index.tsx
+++ igniteteams/src/components/Header/index.tsx
@@ -1,22 +1,26 @@
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 
-import logoImg from '@assets/logo.png';
+import logoImg from "@assets/logo.png";
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
   showBackButton?: boolean;
-}
+};
 
 export function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+  function handleGoBack() {
+    navigation.navigate("groups");
+  }
   return (
     <Container>
-      { 
-        showBackButton &&
-        <BackButton>
+      {showBackButton && (
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
-      }
+      )}
 
       <Logo source={logoImg} />
     </Container>
-  )
+  );
 }
--- igniteteams/src/screens/NewGroup/index.tsx
+++ igniteteams/src/screens/NewGroup/index.tsx
@@ -2,10 +2,17 @@ import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group: "players" });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -16,7 +23,7 @@ export function NewGroup() {
           subtitle="Crie a turma para adicionar as pessoas"
         />
         <Input placeholder="Nome da turma" />
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
