073cb381e5e05ee6596fdd51cbbcb63b04be1f46
Lucca Hora <luccahora@gmail.com>
2022-10-06 19:01:21 -0300

    chore: save data

--- igniteteams/src/screens/NewGroup/index.tsx
+++ igniteteams/src/screens/NewGroup/index.tsx
@@ -1,30 +1,43 @@
-import { Button } from "@components/Button";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+import { groupCreate } from "@storage/group/groupCreate";
+
+import { Container, Content, Icon } from "./styles";
+
 import { Header } from "@components/Header";
+import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-import { useNavigation } from "@react-navigation/native";
-import React, { useState } from "react";
-import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
   const [group, setGroup] = useState("");
 
   const navigation = useNavigation();
 
-  function handleNew() {
-    navigation.navigate("players", { group });
+  async function handleNew() {
+    try {
+      await groupCreate(group);
+      navigation.navigate("players", { group });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <Container>
       <Header showBackButton />
+
       <Content>
         <Icon />
+
         <Highlight
           title="Nova turma"
-          subtitle="Crie a turma para adicionar as pessoas"
+          subtitle="crie a turma para adicionar as pessoas"
         />
+
         <Input placeholder="Nome da turma" onChangeText={setGroup} />
+
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
