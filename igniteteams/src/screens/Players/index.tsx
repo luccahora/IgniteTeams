f7ed62716c6f336ed651b2b2e79e6a7923949c7c
Lucca Hora <luccahora@gmail.com>
2022-10-09 18:22:46 -0300

    chore: list playes

--- igniteteams/src/screens/Players/index.tsx
+++ igniteteams/src/screens/Players/index.tsx
@@ -1,47 +1,82 @@
+import { useState } from "react";
+import { FlatList, Alert } from "react-native";
+import { useRoute } from "@react-navigation/native";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
-
-import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
-import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
-import { FlatList } from "react-native";
-import { useState } from "react";
+import { Input } from "@components/Input";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-import { useRoute } from "@react-navigation/native";
+
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { AppError } from "@utils/AppError";
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroup } from "@storage/player/playersGetByGroup";
 
 type RouteParams = {
   group: string;
 };
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState("");
   const [team, setTeam] = useState("Time A");
   const [players, setPlayers] = useState([]);
 
   const route = useRoute();
+
   const { group } = route.params as RouteParams;
+
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert(
+        "Nova pessoa",
+        "Informe o nome da pessoa para adicionar."
+      );
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    };
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+      const players = await playersGetByGroup(group);
+
+      console.log(players);
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Nova pessoa", error.message);
+      } else {
+        console.log(error);
+        Alert.alert("Nova pessoa", "Não foi possível adicionar.");
+      }
+    }
+  }
 
   return (
     <Container>
       <Header showBackButton />
 
-      <Highlight
-        title={group}
-        subtitle="adicione a galera e separe os times"
-      />
+      <Highlight title={group} subtitle="adicione a galera e separe os times" />
 
       <Form>
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+          onChangeText={setNewPlayerName}
+        />
 
-        <ButtonIcon icon="add" />
+        <ButtonIcon icon="add" onPress={handleAddPlayer} />
       </Form>
+
       <HeaderList>
         <FlatList
           data={["Time A", "Time B"]}
           keyExtractor={(item) => item}
-          horizontal
           renderItem={({ item }) => (
             <Filter
               title={item}
@@ -49,6 +84,7 @@ export function Players() {
               onPress={() => setTeam(item)}
             />
           )}
+          horizontal
         />
 
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
@@ -58,10 +94,10 @@ export function Players() {
         data={players}
         keyExtractor={(item) => item}
         renderItem={({ item }) => (
-          <PlayerCard name={item} onRemove={() => console.log("ss")} />
+          <PlayerCard name={item} onRemove={() => {}} />
         )}
         ListEmptyComponent={() => (
-          <ListEmpty message={"Não há pessoas nesse time."} />
+          <ListEmpty message="Não há pessoas nesse time" />
         )}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={[
@@ -69,6 +105,7 @@ export function Players() {
           players.length === 0 && { flex: 1 },
         ]}
       />
+
       <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
